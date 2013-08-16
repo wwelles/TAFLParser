2c2082dfc0eb05ce560833461b9ad501c6f53a04
Wallace Welles <wwelles@printfleet.com>
2013-08-15 22:33:04 -0400

    no message

--- TAFL/Parser.vb
+++ TAFL/Parser.vb
@@ -9,11 +9,16 @@ Public Class Parser
         DownloadFiles()
 
         For Each item In IO.Directory.GetFiles(IO.Path.GetTempPath(), "*.zip")
+            Debug.WriteLine(String.Format("Unzipping file: {0}", item))
+
             Dim archive = New SevenZipExtractor(item)
             archive.ExtractArchive(IO.Path.GetTempPath())
 
+            Debug.WriteLine(String.Format("Unzipping complete: {0}", item))
+
             Select Case True
                 Case IO.Path.GetFileName(item.ToUpper) = "AMATEUR.ZIP"
+                    Debug.WriteLine(String.Format("Parsing: {0}", item))
                     'ParseFile(item)
 
             End Select
@@ -23,8 +28,13 @@ Public Class Parser
 
     Private Sub DownloadFiles()
         For Each file In Files
-            Dim test = New Net.WebClient
-            test.DownloadFile(file, IO.Path.GetTempPath() & IO.Path.GetFileName(file))
+            Try
+                Dim test = New Net.WebClient
+                test.DownloadFile(file, IO.Path.GetTempPath() & IO.Path.GetFileName(file))
+                Debug.WriteLine(String.Format("Download of {0} successful", file))
+            Catch ex As Exception
+                Debug.WriteLine(String.Format("Download of {0} failed: {1}", file, ex.ToString))
+            End Try
         Next
     End Sub
 

cb3ad5472582a25a3de1feeee10b3cfaa6bf7585
Wallace Welles <wwelles@printfleet.com>
2013-08-16 12:09:22 -0400

    no message

--- TAFL/Parser.vb
+++ TAFL/Parser.vb
@@ -11,24 +11,24 @@ Public Class Parser
     Private amateurSQL = _
         "CREATE TABLE amateur " & _
         "(" & _
-        "  Callsign varchar(6) PRIMARY KEY," & _
-        "  Names varchar(35) NULL," & _
-        "  Surname varchar(35) NULL," & _
-        "  Address varchar(70) NULL," & _
-        "  City varchar(35) NULL," & _
-        "  Province varchar(2) NULL," & _
+        "  Callsign varchar(10) PRIMARY KEY," & _
+        "  Names varchar(40) NULL," & _
+        "  Surname varchar(40) NULL," & _
+        "  Address varchar(75) NULL," & _
+        "  City varchar(40) NULL," & _
+        "  Province varchar(3) NULL," & _
         "  Postal varchar(10) NULL," & _
-        "  BASIC varchar(1) NULL," & _
-        "  [5WPM] varchar(1) NULL," & _
-        "  [12WPM] varchar(1) NULL," & _
-        "  ADVANCED varchar(1) NULL," & _
-        "  BasicHonours varchar(1) NULL," & _
-        "  ClubName1 varchar(70) NULL," & _
-        "  ClubName2 varchar(70) NULL," & _
-        "  ClubAddress varchar(70) NULL," & _
-        "  ClubCity varchar(35) NULL," & _
-        "  ClubProvince varchar(2) NULL," & _
-        "  ClubPostal varchar(7) NULL" & _
+        "  BASIC varchar(2) NULL," & _
+        "  [5WPM] varchar(2) NULL," & _
+        "  [12WPM] varchar(2) NULL," & _
+        "  ADVANCED varchar(2) NULL," & _
+        "  BasicHonours varchar(2) NULL," & _
+        "  ClubName1 varchar(75) NULL," & _
+        "  ClubName2 varchar(75) NULL," & _
+        "  ClubAddress varchar(75) NULL," & _
+        "  ClubCity varchar(40) NULL," & _
+        "  ClubProvince varchar(3) NULL," & _
+        "  ClubPostal varchar(10) NULL" & _
         ");"
 
     Public Sub New()

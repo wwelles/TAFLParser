7a3c95f68836c9ae0f56fbebda796add1c4e8ac3
Wallace Welles <wwelles@printfleet.com>
2013-08-20 09:35:09 -0400

    no message

--- TAFL/Services/Amateur.vb
+++ TAFL/Services/Amateur.vb
@@ -32,27 +32,51 @@ Public Class Amateur
             Dim results = dbConn.Query(String.Format("SELECT * FROM amateur WHERE Callsign LIKE N'%{0}%'", search))
 
             While results.Read()
-                Dim result = CType(results, IDataRecord)
-                Dim record As New TAFL.Models.Amateur
-                
-                record.Callsign = result("Callsign")
-                record.Names = result("Names")
-                record.Surname = result("Surname")
-                record.Address = result("Address")
-                record.City = result("City")
-                record.Province = result("Province")
-                record.Postal = result("Postal")
-                record.ClubName1 = result("ClubName1")
-                record.ClubName2 = result("ClubName2")
-                record.ClubAddress = result("ClubAddress")
-                record.ClubCity = result("ClubCity")
-                record.ClubProvince = result("ClubProvince")
-                record.ClubPostal = result("ClubPostal")
-
-                values.Add(record)
+                values.Add(fillModel(CType(results, IDataRecord)))
             End While
 
             Return values
+
+        End Function
+
+        Public Shared Function LoadByName(search As String) As List(Of TAFL.Models.Amateur)
+            Dim dbConn As New DB
+            Dim values As New List(Of TAFL.Models.Amateur)
+
+            Dim results = dbConn.Query(String.Format("SELECT * FROM amateur WHERE Names LIKE N'%{0}%' OR Surname LIKE N'%{0}%'", search))
+
+            While results.Read()
+                values.Add(fillModel(CType(results, IDataRecord)))
+            End While
+
+            Return values
+
+        End Function
+
+        Private Shared Function fillModel(result As IDataRecord) As TAFL.Models.Amateur
+
+            Dim record As New TAFL.Models.Amateur
+
+            record.Callsign = result("Callsign")
+            record.Names = result("Names")
+            record.Surname = result("Surname")
+            record.Address = result("Address")
+            record.City = result("City")
+            record.Province = result("Province")
+            record.Postal = result("Postal")
+            record.Qualification_BASIC = result("BASIC")
+            record.Qualification_5WPM = result("5WPM")
+            record.Qualification_12WPM = result("12WPM")
+            record.Qualification_ADVANCED = result("ADVANCED")
+            record.Qualification_BasicHonours = result("BasicHonours")
+            record.ClubName1 = result("ClubName1")
+            record.ClubName2 = result("ClubName2")
+            record.ClubAddress = result("ClubAddress")
+            record.ClubCity = result("ClubCity")
+            record.ClubProvince = result("ClubProvince")
+            record.ClubPostal = result("ClubPostal")
+
+            Return record
 
         End Function
 

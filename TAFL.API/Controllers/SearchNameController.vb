83532e966b0a1435248251b433927ff31166362f
Wallace Welles <wwelles@printfleet.com>
2013-08-20 09:39:18 -0400

    no message

---
+++ TAFL.API/Controllers/SearchNameController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Net
+Imports System.Web.Http
+
+Public Class SearchNameController
+    Inherits ApiController
+
+    ' GET api/searchname/Test
+    Public Function GetValues(id As String) As IEnumerable(Of TAFL.Models.Amateur)
+        Return TAFL.Services.Amateur.LoadByName(id)
+    End Function
+
+End Class
